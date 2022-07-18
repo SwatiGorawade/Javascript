14ed77f09bafe8bf2698d946b4e1e458d4e5e459
SwatiGorawade <gorawadeswati@gmail.com>
2022-07-18 17:56:03 +0530

    Second Commit

---
+++ src/javascript/scriptdemo.java
@@ -0,0 +1,12 @@
+package javascript;
+
+public class scriptdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello Demo";
+		System.out.println(str);
+
+	}
+
+}
